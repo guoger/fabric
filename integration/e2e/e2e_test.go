9e5c37ffce4c8370bb81c2a238537e0182a2d856
Jay Guo <guojiannan1101@gmail.com>
2018-06-07 01:13:40 +0800

    WIP
    Change-Id: I76b288bbd59790855f3614acaa608385fc6f2cd1
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- integration/e2e/e2e_test.go
+++ integration/e2e/e2e_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/hyperledger/fabric/integration/helpers"
 	"github.com/hyperledger/fabric/integration/world"
 	"github.com/tedsuo/ifrit"
+	"regexp"
 )
 
 var _ = Describe("EndToEnd", func() {
@@ -38,10 +39,10 @@ var _ = Describe("EndToEnd", func() {
 			Chaincode: world.Chaincode{
 				Name:     "mycc",
 				Version:  "0.0",
-				Path:     "github.com/hyperledger/fabric/integration/chaincode/simple/cmd",
+				Path:     "github.com/hyperledger/fabric-chaincode-evm/plugin",
 				ExecPath: os.Getenv("PATH"),
 			},
-			InitArgs: `{"Args":["init","a","100","b","200"]}`,
+			InitArgs: `{"Args":[]}`,
 			Policy:   `AND ('Org1MSP.member','Org2MSP.member')`,
 			Orderer:  "127.0.0.1:7050",
 		}
@@ -59,7 +60,7 @@ var _ = Describe("EndToEnd", func() {
 			w = world.GenerateBasicConfig("solo", 1, 2, testDir, components)
 		})
 
-		It("executes a basic solo network with 2 orgs", func() {
+		FIt("executes a basic solo network with 2 orgs", func() {
 			By("generating files to bootstrap the network")
 			w.BootstrapNetwork(deployment.Channel)
 			Expect(filepath.Join(testDir, "configtx.yaml")).To(BeARegularFile())
@@ -103,15 +104,35 @@ func RunQueryInvokeQuery(w *world.World, deployment world.Deployment) {
 	adminPeer.LogLevel = "debug"
 	adminPeer.ConfigDir = filepath.Join(w.Rootpath, "peer0.org1.example.com")
 	adminPeer.MSPConfigPath = filepath.Join(w.Rootpath, "crypto", "peerOrganizations", "org1.example.com", "users", "Admin@org1.example.com", "msp")
-	adminRunner := adminPeer.QueryChaincode(deployment.Chaincode.Name, deployment.Channel, `{"Args":["query","a"]}`)
-	execute(adminRunner)
-	Eventually(adminRunner.Buffer()).Should(gbytes.Say("100"))
 
-	By("invoking the chaincode")
+	By("installing smart contract")
+	adminRunner := adminPeer.InvokeChaincode(
+		deployment.Chaincode.Name,
+		deployment.Channel,
+		`{"Args":["0000000000000000000000000000000000000000","6060604052341561000f57600080fd5b60d38061001d6000396000f3006060604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b114604e5780636d4ce63c14606e575b600080fd5b3415605857600080fd5b606c60048080359060200190919050506094565b005b3415607857600080fd5b607e609e565b6040518082815260200191505060405180910390f35b8060008190555050565b600080549050905600a165627a7a72305820122f55f799d70b5f6dbfd4312efb65cdbfaacddedf7c36249b8b1e915a8dd85b0029"]}`,
+		deployment.Orderer,
+		"--waitForEvent",
+		"--peerAddresses", "127.0.0.1:7051",
+		"--peerAddresses", "127.0.0.1:8051",
+	)
+	execute(adminRunner)
+	Eventually(adminRunner.Err()).Should(gbytes.Say("Chaincode invoke successful. result: status:200 payload:\"[a-z0-9]{40}\""))
+
+	re := regexp.MustCompile("payload:\"[a-z0-9]{40}")
+
+	contractAddr := re.FindString(string(adminRunner.Err().Contents()))[9:]
+	By("contract installed at " + string(contractAddr))
+
+	head := `{"Args":["`
+	body := `","`
+	tail := `"]}`
+
+	args := head + string(contractAddr) + body + "60fe47b10000000000000000000000000000000000000000000000000000000000000003" + tail
+	By("invoking the contract with: " + args)
 	adminRunner = adminPeer.InvokeChaincode(
 		deployment.Chaincode.Name,
 		deployment.Channel,
-		`{"Args":["invoke","a","b","10"]}`,
+		args,
 		deployment.Orderer,
 		"--waitForEvent",
 		"--peerAddresses", "127.0.0.1:7051",
@@ -120,18 +141,11 @@ func RunQueryInvokeQuery(w *world.World, deployment world.Deployment) {
 	execute(adminRunner)
 	Eventually(adminRunner.Err()).Should(gbytes.Say("Chaincode invoke successful. result: status:200"))
 
-	By("querying the chaincode again")
-	adminRunner = adminPeer.QueryChaincode(deployment.Chaincode.Name, deployment.Channel, `{"Args":["query","a"]}`)
+	args = head + string(contractAddr) + body + "6d4ce63c" + tail
+	By("querying the chaincode with " + args)
+	adminRunner = adminPeer.QueryChaincode(deployment.Chaincode.Name, deployment.Channel, args)
 	execute(adminRunner)
-	Eventually(adminRunner.Buffer()).Should(gbytes.Say("90"))
-
-	By("updating the channel")
-	adminPeer = components.Peer()
-	adminPeer.ConfigDir = filepath.Join(w.Rootpath, "peer0.org1.example.com")
-	adminPeer.MSPConfigPath = filepath.Join(w.Rootpath, "crypto", "peerOrganizations", "org1.example.com", "users", "Admin@org1.example.com", "msp")
-	adminRunner = adminPeer.UpdateChannel(filepath.Join(w.Rootpath, "Org1_anchors_update_tx.pb"), deployment.Channel, deployment.Orderer)
-	execute(adminRunner)
-	Eventually(adminRunner.Err()).Should(gbytes.Say("Successfully submitted channel update"))
+	Eventually(adminRunner.Buffer()).Should(gbytes.Say("0000000000000000000000000000000000000000000000000000000000000003"))
 }
 
 func execute(r ifrit.Runner) (err error) {
